3dc54df9e548d60f7e35731b2b460ccc5afe8c46
mrx7014 <safen894@gmail.com>
2024-10-24 01:19:11 +0300

    Fixes

--- GC-Setup.sh
+++ GC-Setup.sh
@@ -5,7 +5,16 @@ echo "By: MRX7014"
 sleep 3
 sudo apt-get update -y
 sudo apt-get upgrade -y
-wget -O su.sh https://bit.ly/akuhGet && chmod +x su.sh && ./su.sh
+wget -O akuh.zip https://media.githubusercontent.com/media/akuhnet/wqemu/master/akuh.zip
+unzip akuh.zip
+unzip root.zip
+tar -xvf root.tar.xz
+rm -rf akuh.zip root.zip root.tar.xz
+sleep 1
+echo ""
+echo "Thanks for akuh"
+sleep 2
+clear
 sudo apt-get install build-essential ninja-build bc bison flex libssl-dev
 sudo apt install ccache && export USE_CCACHE=1 && export CCACHE_EXEC=/usr/bin/ccache && ccache -M 50G
 sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick  lib32readline-dev lib32z1-dev liblz4-tool  libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
